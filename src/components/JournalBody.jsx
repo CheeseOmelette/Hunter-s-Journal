f65acaeeb0b9078db9b64da32bc01907afe95b69
CheeseOmelette <poketvgameplays@gmail.com>
2025-11-16 22:43:41 -0300

    Reformulação

--- src/components/JournalBody.jsx
+++ src/components/JournalBody.jsx
@@ -1,11 +1,15 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { JournalFiles } from './JournalFiles';
 import './style.css';
+import { useDraggable } from 'react-use-draggable-scroll';
 
 const JournalBody = () => {
-  const [enemyFile, setEnemyFile] = useState(false); 
-  const [enemyId, setEnemyId] = useState(null); 
+  const [enemyFile, setEnemyFile] = useState(true); 
+  const [enemyId, setEnemyId] = useState(JournalFiles.find(item => item.id === 1)); 
   const [selectedArea, setSelectedArea] = useState(null);
+  const ref = useRef();
+  const { events } = useDraggable(ref);
+
 
   const handleIdClick = (id) => {
     const info = JournalFiles.find((item) => item.id === id);
@@ -23,7 +27,7 @@ const JournalBody = () => {
 
   return (
     <div>
-      <div className="enemies">
+      <div className="enemyList" {...events} ref={ref} >
         {JournalFiles.map((hkEnemy) => (
           <button key={hkEnemy.id} onClick={() => handleIdClick(hkEnemy.id)}  className="enemyButton">
             <img src={hkEnemy.icon} alt={hkEnemy.nome} className="enemyIcon" />
@@ -67,7 +71,7 @@ const JournalBody = () => {
         )}
 
         <footer className="credits">
-  <small>© Team Cherry.</small> 
+  <small>© Team Cherry. </small> 
   <small>All rights belong to them.</small>
   <small><b>Site made by CheeseOmelette</b></small>
   <div className="social-links">
